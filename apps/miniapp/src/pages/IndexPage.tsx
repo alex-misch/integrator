6bdf1be4cdb3af5995b184ff65af9510ba27a617
Aleksandr Mishchenko <h-alex@bk.ru>
2026-01-14 14:00:41 +0300

    redirect

--- apps/miniapp/src/pages/IndexPage.tsx
+++ apps/miniapp/src/pages/IndexPage.tsx
@@ -1,6 +1,14 @@
 import {Page} from '@/components/Layout/Page.tsx';
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export function IndexPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/atlazer/122686', {replace: true});
+  }, [navigate]);
+
   return (
     <Page back={false}>
       <Page.Title />
